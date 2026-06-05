44d6237df1cbb28883028adbbb992ced0907bd09
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-05 20:34:30 +0800

    rename eventmanager to eventbus

--- EventBus.cs
+++ EventBus.cs
@@ -1,7 +1,11 @@
 using Godot;
+using RandomBattles.Fighters;
 using System;
 
 public static class EventBus
 {
+    public static Fighter FighterA;
+    public static Fighter FighterB;
+
     public static Action<string> Hit;
 }
