bfafd9d5cf761b7d6726d55dd8616f2a754536c7
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-05 20:34:10 +0800

    Main camera script init

--- Camera.cs
+++ Camera.cs
@@ -1,27 +1,28 @@
 using Godot;
 using System;
+using RandomBattles.Fighters;
 
 public partial class Camera : Camera2D
 {
   private Fighter _fighter1;
   private Fighter _fighter2;
 
-  private Fighter GetFighter(Fighter.Team fighter)
+  private Fighter GetFighter(Team fighter)
   {
-    return (Fighter)GetTree().GetFirstNodeInGroup(fighter == Fighter.Team.A ? "Team A" : "Team B");
+    return (Fighter)GetTree().GetFirstNodeInGroup(fighter == Team.A ? "Team A" : "Team B");
   }
 
   public override void _Process(double delta)
   {
-    _fighter1 ??= GetFighter(Fighter.Team.A);
-    _fighter2 ??= GetFighter(Fighter.Team.B);
+    _fighter1 ??= GetFighter(Team.A);
+    _fighter2 ??= GetFighter(Team.B);
 
     bool isFighter1Alive = IsInstanceValid(_fighter1);
     bool isFighter2Alive = IsInstanceValid(_fighter2);
     
     if (isFighter1Alive && isFighter2Alive)
     {
-      GlobalPosition = (_fighter1.GlobalPosition + _fighter2.GlobalPosition) * 0.5f;
+      GlobalPosition = new((_fighter1.GlobalPosition.X + _fighter2.GlobalPosition.X) * 0.5f, GlobalPosition.Y);
     }
     else if (isFighter1Alive || isFighter2Alive)
     {
