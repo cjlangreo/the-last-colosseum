99b62312ef6c7b9cc045d058043f10dbd4356fa1
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-05 20:34:04 +0800

    AudioManager init

--- AudioManager.cs
+++ AudioManager.cs
@@ -10,19 +10,28 @@ public partial class AudioManager : Node
     {
         public AudioStreamPlayer HitSoundPlayer;
         public AudioStreamPlayer BlockSoundPlayer;
+        public AudioStreamPlayer SwingSoundPlayer;
     }
+
 
   public override void _EnterTree()
     {
+        ProcessMode = ProcessModeEnum.Always;
+        
         TeamA.HitSoundPlayer = new(){MaxPolyphony = 10};
         TeamA.BlockSoundPlayer = new(){MaxPolyphony = 10};
+        TeamA.SwingSoundPlayer = new(){MaxPolyphony = 10};
 
         TeamB.HitSoundPlayer = new(){MaxPolyphony = 10};
         TeamB.BlockSoundPlayer = new(){MaxPolyphony = 10};
+        TeamB.SwingSoundPlayer = new(){MaxPolyphony = 10};
 
         AddChild(TeamA.HitSoundPlayer);
         AddChild(TeamA.BlockSoundPlayer);
+        AddChild(TeamA.SwingSoundPlayer);
+
         AddChild(TeamB.HitSoundPlayer);
         AddChild(TeamB.BlockSoundPlayer);
+        AddChild(TeamB.SwingSoundPlayer);
     }
 }
