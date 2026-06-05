2e6e8c7a274aedd6fffb339626cb4c31ef17c3cf
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-05 20:32:48 +0800

    script_templates init

---
+++ script_templates/Ability/Default.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class _CLASS_ : Ability
+{
+  public override void InitAbility()
+  {
+    base.InitAbility();
+  }
+  public override void UseAbility()
+  {
+    base.UseAbility();
+  }
+
+  public override void EndAbility()
+  {
+    base.EndAbility();
+  }
+}
