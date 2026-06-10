f158e94d468f4159b421dcc6ea0bfd7608dc5243
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-11 05:09:41 +0800

    also print frametime

--- src/utils/Utils.cs
+++ src/utils/Utils.cs
@@ -9,7 +9,7 @@ public static class Debug
   public static void PrintDebug(string content, string ownerName)
   {
     string currentTime = Time.GetTimeStringFromSystem();
-    GD.PrintRich($"[{currentTime}:{ownerName}] {content}");
+    GD.PrintRich($"[{currentTime}:{Engine.GetProcessFrames()}|{ownerName}] {content}");
   }
 }
 
