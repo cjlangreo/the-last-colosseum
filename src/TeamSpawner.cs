95c93d1602ea2037586073f45e4e42340d7bbfe0
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-06-13 22:06:57 +0800

    handle fighter, weapon, and ability instantiation

--- src/TeamSpawner.cs
+++ src/TeamSpawner.cs
@@ -1,18 +1,71 @@
 using Godot;
+using System;
 using System.Linq;
+using System.Windows.Markup;
+using TheLastColosseum;
+using TheLastColosseum.Abilities;
 using TheLastColosseum.Fighters;
+using TheLastColosseum.Weapons;
+using static TheLastColosseum.Utils.Debug;
 
 [GlobalClass]
 public partial class TeamSpawner : Marker2D
 {
   [Export] public Team Team;
+  [Export] public FighterEnum fighter = FighterEnum.Knight;
+  [Export] public WeaponEnum weapon = WeaponEnum.Greataxe;
+  [Export] public AbilityEnum ability = AbilityEnum.MarkOfTheBear;
+  private Fighter _fighter;
+  private Random _random;
 
   public override void _EnterTree()
   {
-    foreach (Fighter fighter in GetChildren().OfType<Fighter>())
+    _random = new();
+
+    Main main = GetNode<Main>("/root/Main");
+    if (main.Random)
     {
-      fighter.InitTeam(Team);
+      fighter = GetRandomEnumValue<FighterEnum>();
+      weapon = GetRandomEnumValue<WeaponEnum>();
+      ability = GetRandomEnumValue<AbilityEnum>();
     }
+    
+    
+    AddFighter(fighter);
+    AddWeapon(weapon);
+    AddAbility(ability);
+  }
+
+  private void AddFighter(FighterEnum fighter)
+  {
+    FighterStats fighterStats = RefServer.fighters[fighter];
+    _fighter = new()
+    {
+      FighterStats = fighterStats
+    };
+    _fighter.InitTeam(Team);
+    
+    AddChild(_fighter);
+
+    if(Team == Team.A)
+    {
+      RefServer.FighterA = _fighter;
+    } else
+    {
+      RefServer.FighterB = _fighter;
+    }
+  }
+
+  private void AddWeapon(WeaponEnum weapon)
+  {
+    Weapon _weapon = (Weapon)RefServer.weapons[weapon].Duplicate();
+    _fighter.AddChild(_weapon);
+  }
+
+  private void AddAbility(AbilityEnum ability)
+  {
+    Ability _ability = (Ability)RefServer.abilities[ability].Duplicate();
+    _fighter.AddChild(_ability);
   }
 
 }
